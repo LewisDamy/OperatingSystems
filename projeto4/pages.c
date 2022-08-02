7879f4f03a48bb0230bc3b05b430a9a2e90bf468
Isabela <96093473+isasilvac@users.noreply.github.com>
2022-08-02 08:38:22 -0300

    Update pages.c

--- projeto4/pages.c
+++ projeto4/pages.c
@@ -1,164 +1,250 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <time.h>
+#include <stdbool.h>
 
 #define dataSize 52     // quantidade de dados que serao armazenados ao total
-#define memorySize 32   // tamanho da memoria fisica
+#define memorySize 20   // tamanho da memoria fisica
 #define swapSize 20     // tamanho da memoria de swap
 #define amountPages 52  // qntde de paginas
 #define bitsReference 8 // qntde de bits de referencia da pagina
+#define generatedPages 4
+#define referencesQnty 20
+#define totalIterations 10 // qntde total de iterações para o algoritmo rodar
+#define qtMaxReferencias 255
+// qntde de paginas = area de swap + RAM
 
 typedef struct pageNode
 {
-  int index;    // numero da pagina
-  int dataInfo; // informacao sobre o dado requisitado
-  int bitsRef;  // referencias ao longo de 8 ciclos de clock
-  int adress;   // endereco fisico nos quadros de pagina
-  int modified; // checar se foi modificada
-  int inMemory; // checar se esta na memoria
+    int index;     // numero da pagina
+    int reference; // quantidades de referencias
+    int adress;    // endereco fisico nos quadros de pagina
+    int modified;  // checar se foi modificada
+    int inMemory;  // checar se esta na memoria  -> 0 = RAM | -1 = swapMemory
 } page;
 
-page pages[amountPages];    // memoria virtual
-int swapArea[swapSize];     // SWAP
-int memoryArea[memorySize]; // RAM
-char data[dataSize];        // vetor de dados utilizados na aplicacao
-int input[50];              // palavras de no maximo 50 caracteres
-
-void initData(char *dataAux)
-{
-  int i, letter = 122; // 122 corresponde a 'z'
-  for (i = 0; i < dataSize; i++)
-  {
-    if (letter > 96) // preenchendo de z ate a
-    {
-      *(dataAux + i) = letter;
-      letter--;
-    }
-    else
-    {
-      *(dataAux + i) = letter - 6; // o decimal correspondente a
-      letter--;                    // Z comeca em 90 por isso a subtracao
-    }
-  }
-}
-
-void initPaging(int pos, int status, int aux)
-{
-  pages[pos].index = pos;
-  pages[pos].bitsRef = 0;
-  pages[pos].modified = 0;
-  if (status == 0) // se ela esta na memoria
-  {
-    pages[pos].adress = pos;      // pagina de indice 0 correspondera ao quadro 0
-    pages[pos].inMemory = status; // informa se esta na memoria
-    pages[pos].dataInfo = (int)memoryArea[aux];
-  }
-  else
-  {
-    pages[pos].adress = status; // adress recebe -1 por nao estar na memoria
-    pages[pos].inMemory = status;
-    pages[pos].dataInfo = (int)swapArea[aux];
-  }
-}
-
-void initMemory()
-{
-  int i, l;
-  int j;
-  int k = 0;
-  int aux = 0;
-  int aux2 = 0;
-  for (i = 0; i < memorySize; i++) // preenchendo a memoria RAM com os primeiros indices
-  {                                // do vetor data
-    memoryArea[i] = data[i];
-  }
-  for (j = i; j < dataSize; j++) // preenchendo a swap com o restante do vetor data
-  {
-    swapArea[k] = data[j];
-    k++;
-  }
-  for (l = 0; l < amountPages; l++) // preenchendo a memoria virtual
-  {
-    if (l < memorySize)
-    {
-      initPaging(l, 0, aux); // preenchendo com dados em memoria RAM
-      aux++;
-    }
-    else
-    {
-      initPaging(l, -1, aux2); // preenchendo com dados em SWAP area
-      aux2++;
-    }
-  }
-}
+page pages[amountPages];
+int swapArea[swapSize];
+int memoryArea[memorySize];
+char data[dataSize];
+int stringOfReference[referencesQnty];
+int amountAging = 0;
 
 void reader(int events)
 {
-  for (int i = 0; i < events; i++)
-  {
-    // ler a palavra até enter (gerada randomicamente)
-    // colocar em cada indice do vetor input (var global) a letra em int
-  }
+    for (int i = 0; i < events; i++)
+    {
+        // ler a palavra até enter (gerada randomicamente)
+        // colocar em cada indice do vetor input (var global) a letra em int
+    }
 }
 
-void pagesMenaging()
+void initData(char *dataAux)
 {
-  // for do tamanho do vetor input
-  // se letra usada na palavra tiver na memoria bitsRef = 1
-  // se nao tiver sido usada na palavra e tiver na memoria bitsRef = 0
-  // se tiver na palavra e nao tiver na memoria -> aging
+    int i, letter = 122; // 122 corresponde a 'z'
+    for (i = 0; i < dataSize; i++)
+    {
+        if (letter > 96)
+        { // preenchendo de z ate a
+            *(dataAux + i) = letter;
+            letter--;
+        }
+        else
+        {
+            *(dataAux + i) = letter - 6; // o decimal correspondente a
+            letter--;                    // Z comeca em 90 por isso a subtracao
+        }
+    }
 }
 
-void aging()
+void initPaging(page *nodeAux, int pos, int status)
 {
+    nodeAux->index = pos;
+    nodeAux->reference = 0;
+    nodeAux->modified = 0;
+
+    if (status == 0)
+    {                               // se ela esta na memoria
+        nodeAux->adress = pos;      // pagina de indice 0 correspondera ao quadro 0
+        nodeAux->inMemory = status; // informa se esta na memoria
+    }
+    else
+    {
+        nodeAux->adress = status; // adress recebe -1 por nao estar na memoria
+        nodeAux->inMemory = status;
+    }
 }
 
-int main()
+void initMemory(page *nodeAux)
 {
-  int i, p, m, n; // variaveis auxiliares
-  char *dataAux;
-  int events;
-  dataAux = data;
-  initData(dataAux);
-  initMemory();
-  // loop while 1
-  printf("Insira a quantidade de palavras (eventos)\n");
-  scanf("%d", &events);
-  // reader(events);
-  // pagesMenaging;
+    int i, j, l, k = 0;
 
-  /* ------------------- prints de teste ----------------------- */
-  // print do vetor data
-  printf("-------- data ---------\n");
-  for (m = 0; m < dataSize; m++)
-  {
-    printf("%c ", *(dataAux + m));
-  }
+    for (i = 0; i < memorySize; i++)
+    {                            // preenchendo a memoria RAM com os primeiros indices
+        memoryArea[i] = data[i]; // do vetor data
+    }
+    for (j = i; j < dataSize; j++)
+    { // preenchendo a swap com o restante do vetor data
+        swapArea[k] = data[j];
+        k++;
+    }
+    for (l = 0; l < amountPages; l++)
+    { // preenchendo a memoria virtual
+        nodeAux = &pages[l];
+        if (l < memorySize)
+        {
+            initPaging(nodeAux, l, 0); // preenchendo com dados em memoria RAM
+        }
+        else
+        {
+            initPaging(nodeAux, l, -1); // preenchendo com dados em SWAP area
+        }
+    }
+}
 
-  // print da struct pageNode
-  printf("\n-------- pages ---------\n");
-  for (int z = 0; z < amountPages; z++)
-  {
-    printf("index da pag: %d - adress %d - modified %d - inMemory %d\n", pages[z].index, pages[z].adress, pages[z].modified, pages[z].inMemory);
-    printf("conteudo %d\n", pages[z].dataInfo);
-    if (pages[z].adress == -1)
-      printf("sem link\n");
-    printf("%d ", pages[z].bitsRef);
-    printf("\n");
-  }
+void referencesCreator()
+{
+    srand(time(NULL));
+    printf("Generated Numbers: ");
+    for (int i = 0; i < referencesQnty; i++)
+    {
+        stringOfReference[i] = (rand() % (amountPages + 1));
+        printf("%d ", stringOfReference[i]);
+    }
+    printf("%d \n");
+}
 
-  // print da SWAP
-  printf("\n-------- SWAP ---------\n");
-  for (n = 0; n < swapSize; n++)
-  {
-    printf("%c ", swapArea[n]);
-  }
+void bin(unsigned n)
+{ // funcao para imprimir os bits de referencia
+    unsigned i;
+    for (i = 1 << 8; i > 0; i = i / 2)
+    {
+        (n & i) ? printf("1") : printf("0");
+    }
+}
 
-  // print da memoria RAM
-  printf("\n-------- RAM ---------\n");
-  for (p = 0; p < memorySize; p++)
-  {
-    printf("%c ", memoryArea[p]);
-  }
-  return 0;
+int whichIsSmallest(void)
+{                     // procura o menor valor dentre os da memoria principal
+    int temp = 99999; // numero muito alto para ser substituido
+    for (int i = 0; i < memorySize; i++)
+    { // faz interacao na memoria principal
+        if (pages[i].reference < temp)
+        {
+            temp = pages[i].reference;
+        }
+    }
+    return temp;
+}
+
+void searchIndexVirtualMemory(int index)
+{
+    // percorrer o tamanho da memoria virtual e buscar pelo index
+    // quando encontrado a struct page retorna ela
+}
+
+int swapMemoryLocations(int index)
+{ // TODO                 ----- TROCAR AS PAGINAS VITIMAS -----
+    // limpa os bits referenciados
+    // troca a var modified, inMemory = -1
+    return 0;
+    // tras a pagina da memoria swap para a principal modificar os endressos de cada uma
+    // percorrer o tamanho da memoria virtual e buscar pelo index
+    // modificar o inMemory para 0
+    // copia o endereco novo na memoria principal
+
+    // chama pushNonReferedBits
+    // soma + 128 para ja ser referenciada
+}
+
+void pushNonReferedBits(void)
+{ // TODO
+    // Faz interacao em todas as paginas na memoria principal
+    // empurra todos os bits nao referenciados das paginas na
+}
+
+void printBits(int index)
+{
+    // printf("Bits: ");
+    bin(pages[index].reference);
+}
+
+///*
+int agingAlgorithm(int randNum)
+{
+    for (int i = 0; i < amountPages; i++)
+    {
+        if (randNum == pages[i].index && pages[i].inMemory == 0)
+        { // se a pagina for referenciada e se estiver na memoria
+            if (pages[i].reference < qtMaxReferencias)
+            { // checar se não estrapolou a qt max de referencias
+                // "EMPURRA" os bits de referencia, chamando funcao pushNonReferedBits
+                pages[i].reference += 128; // adiciona 1 no bit + significativo
+                printf("\nAFTER:\n");
+                printf("\nrandNum: %i | pages[%i].reference: ", randNum, i);
+                printBits(i);
+                printf("\n");
+            }
+        }
+        else if (pages[i].inMemory == -1)
+        {                                        // se nao tiver na memoria principal
+            int toBeRemoved = whichIsSmallest(); // chama funcao whichIsSmallerst para saber a pagina com menos referencia
+            // chama funcao swapMemoryLocations que limpa os bits antes de ir para a memoria principal
+            // incrementa 1 na quantidade de amountAging
+        }
+        else
+        {
+            // ???
+            continue;
+        }
+    }
+}
+//*/
+
+void pageIterationPrint(int randInt, int z)
+{
+    printf("RAND INT: %d\n", randInt);
+    //   for (int z = 0; z < amountPages; z++) {
+    printf("\n---------PAGE %d---------", pages[z].index);
+    printf("\nBEFORE: ");
+    printf("mod %d | inMemory %d\n", pages[z].modified, pages[z].inMemory);
+    printf("Bits: ");
+    bin(pages[z].reference);
+    printf(" | Decimal: %d\n", pages[z].reference);
+    printf("\nAFTER: ");
+    // pages[z].reference = ;
+    printf("mod %d | inMemory %d\n", pages[z].modified + 1, pages[z].inMemory);
+    printf("Bits: ");
+    bin(pages[z].reference);
+    printf(" | Decimal: %d\n", pages[z].reference);
+    // }
+}
+
+int main(void)
+{
+
+    int i, j, l, k = 0; // variaveis auxiliares
+    int lower = 0;
+    page *nodeAux; //
+    char *dataAux; // pointeiro para data
+    dataAux = data;
+
+    initData(dataAux);
+    initMemory(nodeAux);
+    referencesCreator(lower); // cria a string de referencia à memoria
+
+    // for(int i = 0; i < referencesQnty; i++) {
+    //     pageIterationPrint(stringOfReference[i], i);
+    // }
+
+    // for(int i = 0; i < totalIterations; i++) {
+    //     agingAlgorithm(stringOfReference[i]);
+    // }
+
+    // for(int i = 0; i < referencesQnty; i++) {
+    //     pageIterationPrint(stringOfReference[i], i);
+    // }
+
+    return 0;
 }
